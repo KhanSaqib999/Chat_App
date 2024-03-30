c1f2413cf1ee2f6947fa47ab7370bba1c2d41239
Saqib <tnluser@Ananthanarayanans-MacBook-Pro.local>
2024-03-30 14:35:46 +0530

    health check added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'health/check'
+  get '/health', to: 'health#check'
   post '/login', to: 'users#create_or_find_user'
   patch 'users/update_token', to: 'users#update_token'
   post '/users/delete_token', to: 'users#delete_token'
