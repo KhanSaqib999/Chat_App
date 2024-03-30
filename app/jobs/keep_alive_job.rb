7f595acef6ee23f8f81403b36a65a1b34682a5c5
Saqib <tnluser@Ananthanarayanans-MacBook-Pro.local>
2024-03-30 16:28:53 +0530

    more fixes

--- app/jobs/keep_alive_job.rb
+++ app/jobs/keep_alive_job.rb
@@ -4,8 +4,15 @@ class KeepAliveJob < ApplicationJob
   queue_as :default
 
   def perform
-    response = HTTP.get(ENV['SERVER'] + '/health_check', ssl_context: OpenSSL::SSL::SSLContext.new.tap { |ctx| ctx.set_params(verify_mode: OpenSSL::SSL::VERIFY_NONE) })
-    if response.status.success?
+    uri = URI(ENV['SERVER'] + '/health')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == 'https'
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri)
+    response = http.request(request)
+
+    if response.is_a?(Net::HTTPSuccess)
       Rails.logger.info("Health check successful: #{response}")
     else
       Rails.logger.error("Health check failed: #{response}")
